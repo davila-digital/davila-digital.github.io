b3f9108bd5ccf7284b375eb4747048cdd03d8c95
Eric Davila <erykmynn@gmail.com>
2025-10-13 10:54:19 -0600

    dd_pico theme first run

---
+++ themes/custom/dd_pico/js/dd-pico.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Davila Digital (Pico) behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ddPico = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
